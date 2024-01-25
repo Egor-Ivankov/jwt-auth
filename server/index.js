98d72ae98b80511c51962e64b60ee14e0157fc83
Egor Ivankov <egor.ivankov2015@bk.ru>
2024-01-25 20:30:11 +0300

    fix send message

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const cockieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const router = require('./router/index');
 
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cockieParser());
+app.use(cookieParser());
 app.use(cors());
 app.use('/api', router);
 
