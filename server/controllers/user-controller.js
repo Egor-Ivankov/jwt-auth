1e5f7f419010c4c184c497de835908e6f56a8896
Egor Ivankov <egor.ivankov2015@bk.ru>
2024-01-28 10:41:44 +0300

    add loguot, refresh tokens

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -51,7 +51,10 @@ class UserController {
 
     async logout(req, res, next) {
         try {
-
+            const { refreshToken } = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json(token);
         } catch (e) {
             next(e);
         }
@@ -69,7 +72,17 @@ class UserController {
 
     async refresh(req, res, next) {
         try {
+            const { refreshToken } = req.cookies;
+            const userData = await userService.refresh(refreshToken);
 
+            res.cookie(
+                'refreshToken',
+                userData.refreshToken,
+                {
+                    maxAge: 30 * 24 * 60 * 60 * 1000,
+                    httpOnly: true
+                }
+            );
         } catch (e) {
             next(e);
         }
